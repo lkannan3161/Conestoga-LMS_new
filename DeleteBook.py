ee9dd45ce56b9f5a829f6be27a9d10f1afe905d2
Jan Mari Apodaca <japodaca8833@conestogac.on.ca>
2024-03-20 01:01:52 -0400

    updated theme for EditBook functionality

--- DeleteBook.py
+++ DeleteBook.py
@@ -1,5 +1,5 @@
 import customtkinter
-import tkinter
+import tkinter as tk
 from database import LMS
 from tkinter.messagebox import showerror, showinfo
 import os
@@ -7,22 +7,23 @@ import sys
 
 db = LMS("lms.db")
 
-class DeleteBook(customtkinter.CTkToplevel):
+class DeleteBook(tk.Toplevel):
     def __init__(self, master=None):
         super().__init__(master)
-        self.title("Library Management System")
+        self.title("Delete Book")
+        self.configure(bg="black")
         self.minsize(400,250)
         self.maxsize(400,250)
         self.geometry('400x250')
         self.attributes("-topmost", True)
         
-        heading_frame = customtkinter.CTkFrame(master=self,corner_radius=10)
+        heading_frame = customtkinter.CTkFrame(master=self,corner_radius=10, bg_color="black", fg_color="black")
         heading_frame.pack(padx=10,pady=10, ipadx=20, ipady=5,fill="x",anchor="n")
         
-        label = customtkinter.CTkLabel(master=heading_frame, text="Delete Book",font=customtkinter.CTkFont(family="Robot",size=25, weight="bold"))
+        label = customtkinter.CTkLabel(master=heading_frame, text="Delete Book",font=customtkinter.CTkFont(family="Robot",size=25, weight="bold"), bg_color="black", fg_color="black")
         label.pack(ipady=10)
         
-        main_frame = customtkinter.CTkFrame(master=self,corner_radius=10)
+        main_frame = customtkinter.CTkFrame(master=self,corner_radius=10, bg_color="black", fg_color="black")
         main_frame.pack(padx=10,pady=10, ipadx=5, ipady=5,fill="both",expand=True)
         
         book_id_lbel = customtkinter.CTkLabel(master=main_frame,text="Book ID", font=customtkinter.CTkFont(family="Verdana",size=18))
@@ -31,7 +32,7 @@ class DeleteBook(customtkinter.CTkToplevel):
         self.book_id_input = customtkinter.CTkEntry(master=main_frame, width=200)
         self.book_id_input.pack(padx=5, pady=10)
         
-        delete_book_btn = customtkinter.CTkButton(master=main_frame,text="Delete Book",command=self.delete_book)
+        delete_book_btn = tk.Button(master=main_frame,text="Delete Book", font=("Verdana", 16, "bold"), command=self.delete_book)
         delete_book_btn.pack(padx=10,pady=10)
     
     def delete_book(self):
