83ec182d54a317c65aa817afe681ed5efb4669b7
Jan Mari Apodaca <japodaca8833@conestogac.on.ca>
2024-03-27 04:28:58 -0400

    updated theme and layout of Generate Book Report

--- BookReport.py
+++ BookReport.py
@@ -1,5 +1,5 @@
 import customtkinter
-import tkinter
+import tkinter as tk
 from database import LMS
 from tkinter.messagebox import showerror, showinfo
 from tkinter import filedialog
@@ -9,34 +9,35 @@ import sys
 
 db = LMS ("lms.db")
 
-class BookReport(customtkinter.CTkToplevel):
+class BookReport(tk.Toplevel):
     def __init__(self, master=None):
         super().__init__(master)
-        self.title("Library Management System")
+        self.title("Generate Book Report")
         self.minsize(400,300)
         self.maxsize(400,300)
         self.geometry('400x300')
         self.attributes("-topmost", True)
+        self.configure(bg="black")
         
-        heading_frame = customtkinter.CTkFrame(master=self,corner_radius=10)
+        heading_frame = customtkinter.CTkFrame(master=self,corner_radius=10, bg_color="black", fg_color="black")
         heading_frame.pack(padx=10,pady=10, ipadx=20, ipady=5,fill="x",anchor="n")
         
-        label = customtkinter.CTkLabel(master=heading_frame, text="Generate Book Report",font=customtkinter.CTkFont(family="Robot",size=25, weight="bold"))
+        label = customtkinter.CTkLabel(master=heading_frame, text="Generate Book Report",font=customtkinter.CTkFont(family="Robot",size=25, weight="bold"), bg_color="black", fg_color="black")
         label.pack(ipady=10)
         
-        main_frame = customtkinter.CTkFrame(master=self,corner_radius=10)
+        main_frame = customtkinter.CTkFrame(master=self,corner_radius=10, bg_color="black", fg_color="black")
         main_frame.pack(padx=10,pady=10, ipadx=5, ipady=5,fill="both",expand=True)
         
-        avlb_book_export_btn = customtkinter.CTkButton(master=main_frame,text="Export Available Book",command=self.export_available_book)
+        avlb_book_export_btn = tk.Button(master=main_frame,text="Export Available Book", width = 200, command=self.export_available_book)
         avlb_book_export_btn.pack(padx=10,pady=10)
         
-        issue_book_exp_btn = customtkinter.CTkButton(master=main_frame,text="Export Issued Book",command=self.export_issued_book)
+        issue_book_exp_btn = tk.Button(master=main_frame,text="Export Issued Book", width = 200,command=self.export_issued_book)
         issue_book_exp_btn.pack(padx=10,pady=10)
         
-        export_all_book_btn = customtkinter.CTkButton(master=main_frame,text="Export All Book",command=self.export_all_book)
+        export_all_book_btn = tk.Button(master=main_frame,text="Export All Book", width = 200,command=self.export_all_book)
         export_all_book_btn.pack(padx=10,pady=10)
         
-        export_fine_btn = customtkinter.CTkButton(master=main_frame,text="Export Fine Details",command=self.export_fine_detail)
+        export_fine_btn = tk.Button(master=main_frame,text="Export Fine Details", width = 200,command=self.export_fine_detail)
         export_fine_btn.pack(padx=10,pady=10)
         
     def export_available_book(self):
